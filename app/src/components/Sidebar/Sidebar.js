cfc01468a0cbd0b8a373e8d7badaa6f803cf55e3
Niranjan <niranjanram107@gmail.com>
2024-10-30 15:14:41 -0700

    Add logout

--- app/src/components/Sidebar/Sidebar.js
+++ app/src/components/Sidebar/Sidebar.js
@@ -1,46 +1,46 @@
-import React from 'react'
-import { Header, Icon, Menu, Sidebar, Segment, Image } from 'semantic-ui-react'
-import config from '../../config'
+import React from "react";
+import { Header, Icon, Menu, Sidebar, Segment, Image } from "semantic-ui-react";
+import config from "../../config";
+import { googleLogout } from "@react-oauth/google";
 
-export default ({ profile, openFileUpload, openFolderCreator, openSettings }) => {
-
+export default ({
+  profile,
+  openFileUpload,
+  openFolderCreator,
+  openSettings,
+}) => {
   const signOut = () => {
-    window.gapi.auth2.getAuthInstance().signOut()
-    localStorage.clear()
-    window.location.reload()
-  }
+    googleLogout();
+    localStorage.clear();
+    window.location.reload();
+  };
 
   return (
-    <Sidebar
-      as={Menu}
-      icon='labeled'
-      inverted
-      vertical
-      visible
-      width='thin'
-    >
-      <Header as='h4' color='grey' style={{ margin: '10px auto' }}>{ config.appName }</Header>
-      <Image src={profile.imageUrl} avatar/>
-      <Menu.Item as='a'>
-        <Icon name='folder' />
+    <Sidebar as={Menu} icon="labeled" inverted vertical visible width="thin">
+      <Header as="h4" color="grey" style={{ margin: "10px auto" }}>
+        {config.appName}
+      </Header>
+      <Image src={profile.imageUrl} avatar />
+      <Menu.Item as="a">
+        <Icon name="folder" />
         Files
       </Menu.Item>
-      <Menu.Item as='a' onClick={openFileUpload}>
-        <Icon name='cloud upload' />
+      <Menu.Item as="a" onClick={openFileUpload}>
+        <Icon name="cloud upload" />
         Upload File(s)
       </Menu.Item>
-      <Menu.Item as='a' onClick={openFolderCreator}>
-        <Icon name='plus circle' />
+      <Menu.Item as="a" onClick={openFolderCreator}>
+        <Icon name="plus circle" />
         New Folder
       </Menu.Item>
-      <Menu.Item as='a' onClick={openSettings}>
-        <Icon name='settings' />
+      <Menu.Item as="a" onClick={openSettings}>
+        <Icon name="settings" />
         Options
       </Menu.Item>
-      <Menu.Item as='a' onClick={signOut}>
-        <Icon name='sign-out' />
+      <Menu.Item as="a" onClick={signOut}>
+        <Icon name="sign-out" />
         Sign Out
       </Menu.Item>
     </Sidebar>
-  )
-}
+  );
+};
